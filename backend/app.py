802b10c6c6ddaaaf0d2c4744cd005f78bc2f8109
nano/fmr <fernandomr_@hotmail.com>
2025-01-06 17:48:44 +0100

    Base app - React, Flask, Mysql

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def index():
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState('Loading...');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/')
+      .then(response => response.json())
+      .then(data => {
+        if (data.message) {
+          setMessage(data.message);
+        }else {
+          setMessage('Failed to fetch message.');
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setMessage('Error connecting to backend.');
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My React and Flask App</h1>
         <p>Backend is running on port 5000, and frontend on port 3000.</p>
         <p>Database management is available at port 8080 via phpMyAdmin.</p>
+        
+        <h2>frontend-Backend Communication</h2>
+        <p>{message}</p>
       </header>
     </div>
   );
